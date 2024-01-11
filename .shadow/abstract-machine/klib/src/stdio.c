48d5b4d46c0e4b7fcd0a9c111bf1c260f2d78c51
tracer-nju <tracer@nju.edu.cn>
2024-01-12 01:51:42 +0800

    Linux MacBook-Pro 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  01:51:42 up  3:01,  1 user,  load average: 0.10, 0.03, 0.08

--- .shadow/abstract-machine/klib/src/stdio.c
+++ .shadow/abstract-machine/klib/src/stdio.c
@@ -7,6 +7,79 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 int printf(const char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  char *s;
+  char c;
+  int i;
+  int index;
+  int char_transmitted = 0;
+  char number_buffer[16];
+  while (*fmt)
+    switch (*fmt++)
+    {
+    case '%': /* formatted output */
+      switch (*fmt++)
+      {
+      case 's': /* string */
+        s = va_arg(ap, char *);
+        putstr(s);
+        char_transmitted += strlen(s);
+        break;
+      case 'd': /* int */
+        i = va_arg(ap, int);
+        index = sizeof(number_buffer) - 2;
+        number_buffer[index + 1] = '\0';
+        for (; i; i /= 10)
+        {
+          number_buffer[index] = i % 10 + '0';
+          index--;
+        }
+        putstr(&number_buffer[index + 1]);
+        char_transmitted += strlen(&number_buffer[index + 1]);
+        break;
+      case 'x': /* 16-bit */
+        i = va_arg(ap, int);
+        index = sizeof(number_buffer) - 2;
+        number_buffer[index + 1] = '\0';
+        for (; i; i /= 16)
+        {
+          int v = i % 16;
+          if (v >= 10)
+          {
+            number_buffer[index] = v - 10 + 'a';
+          }
+          else
+          {
+            number_buffer[index] = v + '0';
+          }
+          index--;
+        }
+        putstr("0x");
+        putstr(&number_buffer[index + 1]);
+        char_transmitted += strlen(&number_buffer[index + 1]);
+        break;
+      case 'c': /* char */
+        /* need a cast here since va_arg only
+           takes fully promoted types */
+        c = va_arg(ap, int);
+        putch(c);
+        char_transmitted += 1;
+        break;
+      default:
+        putch('%');
+        putch(*(fmt - 1));
+        panic("Not implemented");
+        break;
+      }
+      break;
+    default:
+      putch(*(fmt - 1));
+      char_transmitted += 1;
+      break;
+    }
+  va_end(ap);
+  return char_transmitted;
   panic("Not implemented");
 }
 
