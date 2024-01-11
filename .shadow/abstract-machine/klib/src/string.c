f90045dae4bfea54b805c6f26671b6db99d8568c
tracer-nju <tracer@nju.edu.cn>
2024-01-12 01:56:02 +0800

    Linux MacBook-Pro 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  01:56:02 up  3:05,  1 user,  load average: 0.24, 0.12, 0.10

--- .shadow/abstract-machine/klib/src/string.c
+++ .shadow/abstract-machine/klib/src/string.c
@@ -5,42 +5,80 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
+  const char *s_tail = s;
+  for (; *s_tail; s_tail++)
+    ;
+  return s_tail - s;
   panic("Not implemented");
 }
 
 char *strcpy(char *dst, const char *src) {
+  for (; *src; src++, dst++)
+    *dst = *src;
+  return dst;
   panic("Not implemented");
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
+  for (; *src && n; src++, dst++, n--)
+    *dst = *src;
+  return dst;
   panic("Not implemented");
 }
 
 char *strcat(char *dst, const char *src) {
+  strcpy(dst + strlen(dst), src);
   panic("Not implemented");
 }
 
 int strcmp(const char *s1, const char *s2) {
+  for (; *s1 == *s2 && *s1; s1++, s2++)
+    ;
+  return *(unsigned char *)s1 - *(unsigned char *)s2;
   panic("Not implemented");
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
+  if (!n--)
+    return 0;
+  for (; *s1 == *s2 && *s1 && n; s1++, s2++, n--)
+    ;
+  return *(unsigned char *)s1 - *(unsigned char *)s2;
   panic("Not implemented");
 }
 
 void *memset(void *s, int c, size_t n) {
+  unsigned char *_s = s;
+  for (; n; n--)
+    *_s = c;
+  return s;
   panic("Not implemented");
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
+  unsigned char *d = dst;
+  const unsigned char *s = src;
+  for (; n; n--)
+    *(unsigned char *)d++ = *(unsigned char *)s++;
+  return dst;
   panic("Not implemented");
 }
 
 void *memcpy(void *out, const void *in, size_t n) {
+  unsigned char *o = out;
+  const unsigned char *i = in;
+  for (; n; n--)
+    *o++ = *i++;
+  return out;
   panic("Not implemented");
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
+  const unsigned char *l = s1;
+  const unsigned char *r = s2;
+  for (; *l == *r && n; n--, l++, r++)
+    ;
+  return n ? *l - *r : 0;
   panic("Not implemented");
 }
 
